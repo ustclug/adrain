1af23c61ca8206deca2a48aed5c80548e70b313f
Weikeng Chen <cwk32@mail.ustc.edu.cn>
2017-02-11 18:52:15 -0800

    Update login.php

--- src/login.php
+++ src/login.php
@@ -7,7 +7,7 @@ $cas_context="";
 require_once './cas-client/CAS.php';
 
 phpCAS::proxy(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
-$url="http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+$url="https://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
 phpCAS::setFixedServiceURL($url);
 phpCAS::setNoCasServerValidation();
 phpCAS::forceAuthentication();
