d477a463ac686f95f82bc62d3577ebedb5fea8dc
Tianyi Cui <1997cui@gmail.com>
2019-05-29 18:04:17 -0700

    Update attachment php

---
+++ src/attachments/cs-merge.php
@@ -0,0 +1,12 @@
+<?php
+
+if(login()){
+    header("Content-type:application/pdf");
+    // It will be called downloaded.pdf
+    header("Content-Disposition:attachment;filename='cs.pdf'");
+    // The PDF source is in original.pdf
+    readfile("/srv/priv/cs-merge.pdf");
+} else
+{
+    <p><a href="../plogin.php">登录</a></p>
+}
