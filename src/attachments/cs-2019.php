6fa77f05890a8ac66bb22fb2d8f01d976591524a
taoky <taoky1999@gmail.com>
2022-01-20 00:54:40 +0800

    Fix attachments when file does not exist

--- src/attachments/cs-2019.php
+++ src/attachments/cs-2019.php
@@ -5,11 +5,17 @@ function login()
     return $_SESSION['userid'];
 }
 if (login()) {
+    $filename = "/srv/priv/cs-merge.pdf";
+    if (!file_exists($filename)) {
+        http_response_code(404);
+        echo "File not exist. Please contact the administrator.";
+        exit();
+    }
     header("Content-type:application/pdf");
     // It will be called downloaded.pdf
     header("Content-Disposition:attachment;filename=cs-2019.pdf");
     // The PDF source is in original.pdf
-    readfile("/srv/priv/cs-merge.pdf");
+    readfile($filename);
 } else {
     print "<p><a href=\"../plogin.php\">登录</a></p>";
 }
--- src/attachments/cs-2020.php
+++ src/attachments/cs-2020.php
@@ -5,11 +5,17 @@ function login()
     return $_SESSION['userid'];
 }
 if (login()) {
+    $filename = "/srv/priv/cs-2020.pdf";
+    if (!file_exists($filename)) {
+        http_response_code(404);
+        echo "File not exist. Please contact the administrator.";
+        exit();
+    }
     header("Content-type:application/pdf");
     // It will be called downloaded.pdf
     header("Content-Disposition:attachment;filename=cs-2020.pdf");
     // The PDF source is in original.pdf
-    readfile("/srv/priv/cs-2020.pdf");
+    readfile($filename);
 } else {
     print "<p><a href=\"../plogin.php\">登录</a></p>";
 }
